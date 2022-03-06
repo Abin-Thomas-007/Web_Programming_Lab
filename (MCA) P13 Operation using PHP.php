26dbe24a0ea4ed0461013a2d8e584c3373420fac
Richu Antony <96441125+Asseno@users.noreply.github.com>
2022-03-06 11:00:07 +0530

    Final fix

--- (MCA) P13 Operation using PHP.php
+++ (MCA) P13 Operation using PHP.php
@@ -1,4 +1,112 @@
-<!-- WRITE A PROGRAM IN PHP TO CHECK WHETHER A NUMBER IS PALINDROME OR NOT.. -->
+<!-- PHP SAME PROGRAM FOR INSERTION, UPDATION, DELETION AND RETRIEVING RECORDS. -->
+
+<?php
+require('Connection.php')
+?>
+
+<!-- Select PHP Code -->
+<?php
+$num_roll ="";
+$txt_name=$txt_course="";
+if (isset($_POST['select'])) {
+    $roll = $_POST['roll'];
+    $select_query = "SELECT ROLL_NO, FULL_NAME, COURSE FROM student WHERE ROLL_NO='$roll' ";
+    $select_result = mysqli_query($con, $select_query);
+    if (mysqli_num_rows($select_result) > 0) 
+    {
+        while ($srow = mysqli_fetch_array($select_result)) 
+        {
+            $num_roll = $srow['ROLL_NO'];
+            $txt_name = $srow['FULL_NAME'];
+            $txt_course = $srow['COURSE'];
+        }
+    } 
+    else
+    {
+        $msg = "NO Results Where Found!!!<br>";
+    }
+    mysqli_close($con);
+}
+?>
+
+<!-- Insert PHP Code -->
+<?php
+if (isset($_POST['insert'])) 
+{
+    $roll = $_POST['roll'];
+    $name = $_POST['name'];
+    $course = $_POST['course'];
+    $insert_query = "INSERT INTO student VALUES('$roll','$name','$course')";
+    $insert_result = mysqli_query($con, $insert_query);
+    if (!$insert_result) 
+    {
+        $msg = "Insertion Error !!!" . mysqli_error($con);
+    } 
+    else
+    {
+         $msg = "Data Insertion Success!!!";
+    }
+    mysqli_close($con);
+}
+?>
+
+<!-- Update PHP Code -->
+<?php
+if (isset($_POST['update'])) 
+{
+    $roll = $_POST['roll'];
+    $name = $_POST['name'];
+    $course = $_POST['course'];
+    $update_query = "UPDATE student SET FULL_NAME='$name',COURSE='$course' WHERE ROLL_NO='$roll'";
+    $update_result = mysqli_query($con, $update_query);
+    if (!$update_result) 
+    {
+        $msg = "Updation Error !!!" . mysqli_error($con);
+    } 
+    else
+    {
+        $msg = "Data Updation Success!!!";
+    }    
+    mysqli_close($con);
+}
+?>
+
+<!-- Delete PHP Code -->
+<?php
+if (isset($_POST['delete'])) 
+{
+    $roll = $_POST['roll'];
+    $delete_query = "DELETE FROM student WHERE ROLL_NO='$roll'";
+    $delete_result = mysqli_query($con, $delete_query);
+    if (!$delete_result) 
+    {
+        $msg = "Deletion Error !!!" . mysqli_error($con);
+    } 
+    else
+    {
+        $msg = "Data Deletion Success!!!";
+    }
+    mysqli_close($con);
+}
+?>
+
+<!-- Select all PHP Code -->
+<?php
+if (isset($_POST['selectall'])) 
+{
+    $selectall_query = "SELECT ROLL_NO, FULL_NAME, COURSE FROM student";
+    $select_result = mysqli_query($con, $selectall_query);
+    if (mysqli_num_rows($select_result) > 0) 
+    {
+        while ($asrow = mysqli_fetch_assoc($select_result)) 
+        {
+            echo "ID: {$asrow['ROLL_NO']} <br>" . "Name: {$asrow['FULL_NAME']} <br>" . "Salary : {$asrow['COURSE']} <br>" .
+                "----------------------------------<br>";
+        }
+    }
+    mysqli_close($con);
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -7,94 +115,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Opertions Using PHP</title>
+    <link rel="icon" href="Images/lightning.png" type="image/x-icon">
+    <style>
+        *{
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+            font-size: 24px;
+        }
+
+        body {
+            background-image: url(Images/Ballon_sky.jpg);
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+            font-size: larger;
+            text-align: center;
+        }
+    </style>
 </head>
 
-<body>
-    <?php
-    $num_id;
-    $txt_name = $txt_sal = "";
-    $con = mysqli_connect('localhost:3306', 'root', '', 'accounts');
-    if (!$con)
-        die("Failed to connect: " . mysqli_connect_error());
-    $msg = "Connection Sucess!!!<br>";
-    if (isset($_POST['select'])) {
-        $id = $_POST['num_id'];
-        $sql = "SELECT id,name,salary FROM salary WHERE id='$id'";
-        $result = mysqli_query($con, $sql);
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_array($result)) {
-                $num_id = $row['id'];
-                $txt_name = $row['name'];
-                $txt_sal = $row['salary'];
-            }
-        } else
-            $msg = "Zero Results!!!<br>";
-        mysqli_close($con);
-    }
-    if (isset($_POST['insert'])) {
-        $id = $_POST['num_id'];
-        $name = $_POST['txt_name'];
-        $salary = $_POST['txt_sal'];
-        $sql_1 = "INSERT INTO salary VALUES('$id','$name','$salary')";
-        $result_1 = mysqli_query($con, $sql_1);
-        if (!$result_1) {
-            $msg = "Error !!!" . mysqli_error($con);
-        } else
-            $msg = "Insertion Success!!!";
-        mysqli_close($con);
-    }
-    if (isset($_POST['update'])) {
-        $id = $_POST['num_id'];
-        $name = $_POST['txt_name'];
-        $salary = $_POST['txt_sal'];
-        $sql_2 = "UPDATE salary SET name='$name',salary='$salary' WHERE id='$id'";
-        $result_2 = mysqli_query($con, $sql_2);
-        if (!$result_2) {
-            $msg = "Error !!!" . mysqli_error($con);
-        } else
-            $msg = "Updation Success!!!";
-        mysqli_close($con);
-    }
-    if (isset($_POST['delete'])) {
-        $id = $_POST['num_id'];
-        $sql_3 = "DELETE FROM salary WHERE id='$id'";
-        $result_3 = mysqli_query($con, $sql_3);
-        if (!$result_3) {
-            $msg = "Error !!!" . mysqli_error($con);
-        } else
-            $msg = "Deletion Success!!!";
-        mysqli_close($con);
-    }
-    ?>
-    <h1>Demonstration of PHP MYSQL</h1>
+<body align="center">
+    <h1>Student - Demonstration of PHP MYSQL</h1>
+    <br><br>
     <form method="post">
-        ID:<input type="number" name="num_id" value="<?php echo $num_id; ?>"><br>
-        Name:<input type="text" name="txt_name" value="<?php echo $txt_name; ?>"><br>
-        Salary:<input type="text" name="txt_sal" value="<?php echo $txt_sal; ?>"><br>
+        <label>Roll No:
+            &nbsp;<input type="number" name="roll" value="<?php echo $num_roll; ?>">
+        </label> <br><br>
+        <label>Name:
+            <input type="text" name="name" value="<?php echo $txt_name; ?>">
+        </label><br><br>
+        <label>Course:
+            <input type="text" name="course" value="<?php echo $txt_course; ?>">
+        </label> <br><br>
         <input type="submit" value="SELECT" name="select">
         <input type="submit" value="INSERT" name="insert">
         <input type="submit" value="UPDATE" name="update">
         <input type="submit" value="DELETE" name="delete">
         <input type="submit" value="SELECT ALL" name="selectall">
         <input type="reset" value="RESET" name="reset">
-        <?php echo $msg; ?>
+        <br><br><br>
+        <p>
+            <?php echo $msg; ?>
+        </p>
     </form>
-    <?php
-    if (isset($_POST['selectall'])) {
-        $sql_4 = "SELECT id,name,salary FROM salary";
-        $result_4 = mysqli_query($con, $sql_4);
-        if (mysqli_num_rows($result_4) > 0) {
-            while ($row = mysqli_fetch_assoc($result_4)) {
-                echo "ID: {$row['id']} <br>" .
-                    "Name: {$row['name']} <br>" .
-                    "Salary : {$row['salary']} <br>" .
-                    "----------------------------------<br>";
-            }
-        }
-        mysqli_close($con);
-    }
-    ?>
-
 </body>
 
 </html>
